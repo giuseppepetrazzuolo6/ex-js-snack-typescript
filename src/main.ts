1aa07cc5c1b7afad9bcc61a1906197e827ca14dd
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-03-03 10:42:34 +0100

    snack-3

--- src/main.ts
+++ src/main.ts
@@ -58,3 +58,30 @@ const utenteDipendente: Dipendente = {
     contratto: 'freelance'
 }
 console.log(utenteDipendente)
+
+/*🏆 Snack 3
+Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+
+Developer
+livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+ProjectManager
+teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).*/
+
+
+//creo nuovo alias Developer
+type Developer = Dipendente & {
+    livelloEsperienza: 'Junior' | 'Mid' | 'Senior',
+    linguaggi?: string[],
+    certificazioni: string[]
+}
+
+//creo un alias per ProjectManager
+type ProjectManager = Dipendente & {
+    teamSize: number | null,
+    budgetGestito?: number,
+    stakeholderPrincipali: string[]
+}
